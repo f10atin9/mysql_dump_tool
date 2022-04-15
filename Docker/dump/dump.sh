b5fb2f3b4dbb1af7afce2adab3fd91ffb89a4fc0
f10atin9 <f10atin9@kubesphere.io>
2022-04-15 15:08:28 +0800

    fix: Solved the problem of connecting to mysql and optimized the directory structure

--- Docker/dump/dump.sh
+++ Docker/dump/dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DB_USER=${Mysql-User:-${MYSQL_ENV_DB_USER}}
+DB_USER=${Mysql_User:-${MYSQL_ENV_DB_USER}}
 DB_PASS=${Mysql_Password:-${MYSQL_ENV_DB_PASS}}
 DB_NAME=${Mysql_DBName:-${MYSQL_ENV_DB_NAME}}
 DB_HOST=${Mysql_Host:-${MYSQL_ENV_DB_HOST}}
@@ -22,21 +22,16 @@ if [[ ${DB_HOST} == "" ]]; then
         exit 1
 fi
 
-DIR_NAME="`date +%Y%m%d%H`"
-mkdir /mysqldump/${DIR_NAME}
+DIR_PATH="`date +%Y/%m/%d/%H`"
+TOTAL_PATH=${Local_Path}/mysqldump/${DIR_PATH}
+if [ ! -d "${TOTAL_PATH}" ]; then
+    mkdir -p ${TOTAL_PATH}
+fi
 
-if [[ ${ALL_DATABASES} == "" ]]; then
-        if [[ ${DB_NAME} == "" ]]; then
-                echo "Missing DB_NAME env variable"
-                exit 1
-        fi
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --port="${DB_PORT}" "$@" "${DB_NAME}" > /mysqldump/${DIR_NAME}/"${DB_NAME}".sql
-else
-        databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+databases=`mysql -u${DB_USER} -p${DB_PASS} -h${DB_HOST} -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
         echo "Dumping database: $db"
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --port="${DB_PORT}" --databases $db > /mysqldump/${DIR_NAME}/$db.sql
+        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --port="${DB_PORT}" --databases $db > ${TOTAL_PATH}/$db.sql
     fi
 done
-fi
